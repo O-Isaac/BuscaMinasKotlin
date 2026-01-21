ea87f5914d8edcdfee2b17a92a8bab33569636c0
O-Isaac <zaragozamendozaisaac@outlook.es>
2026-01-21 22:04:01 +0100

    feat(navigation): implement RootNode with Appyx BackStack and refactor MainActivity to use NodeHost

--- app/src/main/java/com/github/isaac/buscaminaskotlin/MainActivity.kt
+++ app/src/main/java/com/github/isaac/buscaminaskotlin/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.github.isaac.buscaminaskotlin
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.WindowCompat.enableEdgeToEdge
-import com.bumble.appyx.core.integration.NodeHost
-import com.github.isaac.buscaminaskotlin.navigation.NavigationActivity
-import com.github.isaac.buscaminaskotlin.navigation.RootNode
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.bumble.appyx.navigation.integration.NodeActivity
+import com.bumble.appyx.navigation.integration.NodeHost
+import com.bumble.appyx.navigation.platform.AndroidLifecycle
+import com.github.isaac.buscaminaskotlin.navigation.root.RootNode
 import com.github.isaac.buscaminaskotlin.ui.theme.BuscaMinasKotlinTheme
 
-class MainActivity : NavigationActivity() {
+class MainActivity : NodeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BuscaMinasKotlinTheme {
-                NodeHost(integrationPoint = appyxV1IntegrationPoint) {
-                    RootNode(buildContext = it)
+                NodeHost(
+                    lifecycle = AndroidLifecycle(LocalLifecycleOwner.current.lifecycle),
+                    integrationPoint = appyxIntegrationPoint
+                ) {
+                    RootNode(nodeContext = it)
                 }
             }
         }
